9435faf3bc28ce54505cce7948ce514a8e935fa5
Henrique Melanda <hjdesigner23@gmail.com>
2020-04-06 15:11:59 -0300

    Logando o usuário com localStorage

--- js/mural/Mural.js
+++ js/mural/Mural.js
@@ -6,15 +6,19 @@ const Mural = (function(_render, Filtro){
     Filtro.on("filtrado", render)
 
     function adiciona(cartao){
-        cartoes.push(cartao)
+			if (logado) {
+				cartoes.push(cartao)
         cartao.on("mudanca.**", render)
         cartao.on("remocao", ()=>{
-            cartoes = cartoes.slice(0)
-            cartoes.splice(cartoes.indexOf(cartao),1)
-            render()
+        	cartoes = cartoes.slice(0)
+          cartoes.splice(cartoes.indexOf(cartao),1)
+          render()
         })
         render()
         return true
+			} else {
+				alert("Você não esta logado");
+			}
     }
 
     return Object.seal({
